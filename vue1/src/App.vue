10010b4f9f994ce2d2d34922493e0c773502d5cb
Dmitriy <dmitryweazy@gmail.com>
2020-04-30 12:10:05 +0300

    Добавлена кнопка смены темы

---
+++ vue1/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <toggle-button :value="false" :labels="{checked: 'ON', unchecked: 'OFF'}" :width="75" :height="33" :color="{checked: '#2F3136', unchecked: '#BCE0FD'}" :font-size="12"/>
+</template>
+<style lang="sass" scoped>
+span 
+  font-family: sans-serif
+h1 
+  color: blue
+  margin: 0
+</style>
+
