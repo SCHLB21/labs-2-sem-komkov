e597c84b0bf4073cde5c62f528f590ba9b1baa41
Dmitriy <dmitryweazy@gmail.com>
2020-05-01 22:19:44 +0300

    1.2

---
+++ vue1/src/AddCard.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="add-card container">
+        <span>Добавить новую задачу</span>
+        <div class="add-card__field">
+            <input type="text" placeholder="Описание">
+            <button>+</button>
+        </div>
+    </div>
+</template>
--- vue1/src/App.vue
+++ vue1/src/App.vue
@@ -1,11 +1,34 @@
 <template>
-  <toggle-button :value="false" :labels="{checked: 'ON', unchecked: 'OFF'}" :width="75" :height="33" :color="{checked: '#2F3136', unchecked: '#BCE0FD'}" :font-size="12"/>
+  <div>
+    <header>
+        <div class="header-wrap container">
+            <div class="header__logo">
+              <h1>Канбан</h1>
+            </div>
+            <!-- /.header__logo -->
+            <div class="header__theme">
+              <span>Темная тема</span>
+              <toggle-button :value="false" :labels="{checked: 'ON', unchecked: 'OFF'}" :width="70" :height="33" :color="{checked: '#2F3136', unchecked: '#BCE0FD'}" :font-size="12"/>
+            </div>
+            <!-- /.header__theme -->
+        </div>
+      </header>
+      <main>
+        <add-card></add-card>
+      </main>
+  </div>
 </template>
-<style lang="sass" scoped>
-span 
-  font-family: sans-serif
-h1 
-  color: blue
-  margin: 0
+<script>
+  import taskCard from './Card.vue';
+  import addCard from './AddCard.vue';
+  export default {
+    components: {
+      taskCard: taskCard,
+      addCard: addCard
+    }
+  }
+</script>
+<style lang="sass">
+@import '../sass/style.sass';
 </style>
 
